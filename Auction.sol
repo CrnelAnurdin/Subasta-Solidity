2f9b9a545161d8f4b181341f0e19d60d5c53fff0
CrnelAnurdin <mmdiaz03@gmail.com>
2024-11-09 23:36:19 -0300

    terminado

--- Auction.sol
+++ Auction.sol
@@ -2,47 +2,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity > 0.8.0;
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol"; trate de importar el ownable pero me tiraba un error el constructor
 
-contract Auction{ //is Ownable {    
-    uint private FloorValue ; //Valor inicial
-    uint private StartDate  ;         //Fecha de inicio
-    uint private Timeframe ;          //Duracion de subasta
+contract Auction  { 
+    //Inicio Variables Entorno
+    uint private FloorValue ; 
+    uint private StartDate  ; 
+    uint private Timeframe ;  
+    uint public  offerWinAmount;  
     struct Offer {
         uint     date;        
         uint     offerAmount; 
         uint     excessAmount;              
     }
-    address  offerWinAddress;  //Direccion ganando   
-    uint     offerWinAmount;   //Oferta ganando      
-    mapping(address => Offer) private offers;
+    address  public offerWinAddress; 
+    address  private owner;
+    enum Estado{ Activa, Cerrada, Pausada
+    }    
+    Estado public estado;
     address[] private offerAddresses; //Lista de direcciones ofertantes para poder recorrerlas y mostrar todas las ofertas
+    mapping(address => Offer) private offers; //Mapeo una direccion a una oferta con la estructura de Offer    
+
+    //Fin Variables Entorno
+
 
     constructor () {
         FloorValue = 10 gwei;
         StartDate = block.timestamp;
-        Timeframe = StartDate + 7 days;
+        Timeframe = StartDate + 7 days;        
+        owner = msg.sender;
+        estado = Estado.Activa;        
     }
+
+    //Inicio Eventos
     event NewBid(address indexed bidder, uint amount);
     event EndAuction (string text);
     event AuctionExtended(uint newTimeframe);
-    function AuctionEnded() internal {
-       uint amountReturn;
-       address addresReturn;
-       require(block.timestamp >= Timeframe, "La subasta sigue vigente");
-       for (uint i = 0; i < offerAddresses.length; i++) {
-        if (offerAddresses[i] != offerWinAddress) {           
-            amountReturn = offers[addresReturn].offerAmount *98 / 100;
-            if (amountReturn == 0) {
-                 continue; // Salteo ya devolvi o ya retiro
-            }
-           addresReturn = offerAddresses[i];           
-           offers[addresReturn].offerAmount = 0;
-           payable(addresReturn).transfer(amountReturn);
-        }
+    event Withdraw (address indexed bidder, uint amount);
+    //Fin Eventos
+
+    //Inicio modificadores
+    modifier withinWithdrawalWindow(uint _deadline) {   //agrega un limite de tiempo al retiro
+        require(block.timestamp <= _deadline, "El tiempo de retiro manual ha expirado");
+        _;
     }
-        emit EndAuction("La subasta ha terminado");
+    modifier onlyAfterAuction() { //Solo si termino la subasta
+        require(block.timestamp >= Timeframe, "La subasta sigue vigente");
+    _;
     }
+    modifier onlyOwner() { 
+        require(msg.sender == owner, "Usted no tiene permisos");
+        _;
+    }
+
+    modifier onlyWinner (){
+        require(msg.sender == offerWinAddress, "No gano la subasta no puede claimear el item subastado");
+        _;
+     }
+    modifier onlyNotWinner (){
+        require(msg.sender != offerWinAddress, "Gano la subasta no puede retirar la oferta");
+        _;
+     }
+     modifier onlyActive (){        
+        require(estado == Estado.Activa, "La subasta ha terminado o se encuentra pausada");  //Valido si esta activa la subasta
+        _;
+     }
 
      modifier extendAuctionIfNeeded() {     
         if (block.timestamp >= Timeframe - 10 minutes) {
@@ -51,24 +75,53 @@ contract Auction{ //is Ownable {
         }
         _;
     }
-     
-     
+    //Fin modificadores
 
-    function setOffer () external payable extendAuctionIfNeeded {        
-      require(block.timestamp <= Timeframe, "La subasta ha terminado");  //Valido si esta activa la subasta
+    //Inicio funciones
+    function manualWithdraw() external  onlyAfterAuction withinWithdrawalWindow(Timeframe + 30 days) onlyNotWinner {
+        uint amount = offers[msg.sender].offerAmount;
+        require(amount > 0, "No tienes monto a retirar");
+        offers[msg.sender].offerAmount = 0;  
+        uint refundAmount = amount * 98 / 100;
+        payable(msg.sender).transfer(refundAmount);
+        emit Withdraw(msg.sender,refundAmount);
+    }
+
+    function bulkWithdraw() internal onlyAfterAuction onlyOwner  { //devuelvo los retiros sin reclamar pasado los x dias definidos para retiro manual
+        for (uint i = 0; i < offerAddresses.length; i++) {
+            address addresReturn = offerAddresses[i];
+            uint amount = offers[addresReturn].offerAmount;        
+            if (amount > 0 && offerWinAddress != addresReturn) {
+                offers[addresReturn].offerAmount = 0;  
+                uint refundAmount = amount * 98 / 100;
+                payable(addresReturn).transfer(refundAmount);
+            }
+        }
+        emit EndAuction("La subasta termino devuelvo retiro sin reclamar.");
+    }
+
+
+    function AuctionEnded() external onlyOwner {
+        require(block.timestamp >= Timeframe, "La subasta sigue vigente");       
+        estado = Estado.Cerrada;
+        emit EndAuction("La subasta ha terminado");
+    }     
+     
+    function setOffer () external payable extendAuctionIfNeeded onlyActive {              
+      require(msg.sender != owner, "El propietario no puede ofertar en su propia subasta");
       if (offers[msg.sender].offerAmount == 0) {    //Verifico si ya realizo una oferta esa address
             offerAddresses.push(msg.sender);
         } else {
-        //  Me fijo si ya esta ganando la subasta
-        require(msg.sender != offerWinAddress, "Ya tienes la oferta mas alta");
+        //  Me fijo si ya esta ganando la subasta o si es el owner
+        require(msg.sender != offerWinAddress , "Ya tienes la oferta mas alta");
       }
       require(msg.value >= offerWinAmount *105/100, "La oferta debe ser superior al menos en 5%");     //Verifico si la oferta es valida
       offers[msg.sender] = Offer({
             date: block.timestamp,
             offerAmount: msg.value,
-            excessAmount: offers[msg.sender].offerAmount +  offers[msg.sender].excessAmount
+            excessAmount: offers[msg.sender].offerAmount > 0 ? offers[msg.sender].excessAmount + offers[msg.sender].offerAmount : 0
         });       //Registro la oferta, y actualizo el excess si ya realizo oferta para que pueda retirar
-        offerWinAmount = msg.value;   //Actualizo la oferta ganadora y la address podria hacerlo en una funcion 
+        offerWinAmount = msg.value;   //Actualizo la oferta ganadora y la address podria hacerlo en una funcion aparte
         offerWinAddress = msg.sender; //y recorrer todas las address comparando las ofertas pero tendria que llamarla cada vez para los dos require de arriba
         emit NewBid(msg.sender, msg.value);        
 
@@ -86,17 +139,19 @@ contract Auction{ //is Ownable {
         address[] memory offerors = new address[](offerAddresses.length);
         uint[] memory amounts = new uint[](offerAddresses.length);
         for (uint i = 0; i < offerAddresses.length; i++) {
-        address offeror = offerAddresses[i];
-        offerors[i] = offeror;
-        amounts[i] = offers[offeror].offerAmount;
+            address offeror = offerAddresses[i];
+            offerors[i] = offeror;
+            amounts[i] = offers[offeror].offerAmount;
         }    
         return (offerors, amounts); 
     }
 
     function withdrawExcess() external payable {
-        require(msg.value<=offers[msg.sender].excessAmount,"No tiene suficiente saldo para retirar");
-        offers[msg.sender].excessAmount = offers[msg.sender].excessAmount-msg.value;
-        payable(msg.sender).transfer(msg.value);
+        uint amount = offers[msg.sender].excessAmount;
+        require(amount>0,"No tiene suficiente saldo para retirar");
+        offers[msg.sender].excessAmount = 0;
+        payable(msg.sender).transfer(amount);
+        emit Withdraw(msg.sender,amount);
     }
     function getExcess() external view  returns(uint){
         return (offers[msg.sender].excessAmount);
@@ -115,6 +170,18 @@ contract Auction{ //is Ownable {
         _hours = timeLeft / 3600;             // Calculo las horas (1 hora = 3600 seconds)
         timeLeft %= 3600;                    // Actualizo el timeleft con el resto de segundos que quedan
 
-        _minutes = timeLeft / 60;             // Calculate minutes (1 minute = 60 seconds)
+        _minutes = timeLeft / 60;             // Calculate minutes (1 minute = 60 seconds)        
     }
+
+    function claimItem() external onlyWinner onlyAfterAuction {
+        require(estado == Estado.Cerrada, "La subasta no ha finalizado");    
+        // Aca se haria la transferencia de ownership del item en cuestion
+
+        emit EndAuction("El ganador reclamo el item");
+    }
+    function finalizeWithdrawals() external  onlyOwner onlyAfterAuction  {
+        require(block.timestamp > Timeframe + 30 days, "Todavia esta habilitado el retiro manual");
+        bulkWithdraw(); 
+    }
+    //Fin funciones
 }
